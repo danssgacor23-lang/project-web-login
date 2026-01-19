b39cfc7b75dd51beb0e6e6ff51d4aa6e76b95702
danssgacor23-lang <danssgacor23@gmail.com>
2026-01-19 15:54:46 +0700

    Upload page.jsx

---
+++ app/admin/dashboard/page.jsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Admin Dashboard</h2>
+      <a href="/admin/create">➕ Create User</a>
+    </div>
+  );
+}
