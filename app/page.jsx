d9fd32f799bc3deecd2b60fb4ae44fcd5e4f9254
danssgacor23-lang <danssgacor23@gmail.com>
2026-01-19 16:02:50 +0700

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "Arial" }}>
+      <h2>Project Web Login</h2>
+      <p>Server is running.</p>
+      <a href="/admin/login">Go to Admin Login</a>
+    </div>
+  );
+}
